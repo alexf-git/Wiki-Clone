d97f38d152484a6f87e94ec9d0001ae9abfb1181
memacias <memacias@google.com>
2021-05-26 01:13:51 +0000

    Step 1 lol

--- wiki.py
+++ wiki.py
@@ -5,17 +5,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return handle_request('FrontPage.txt')
+    return handle_request('pages/FrontPage.txt')
 
-@app.route("pages/<this_page>")
-def handle_request(this_page=None):                  # receive the request
+@app.route("/handle_request/")
+@app.route("/handle_request/<this_page>")
+def handle_request(this_page):                  # receive the request
     # TODO: load the desired page content
+    raw = this_page.split('/')
+    name = raw[-1]
+    name = name.strip('.txt')
     payload = None
     with open(this_page, 'r') as f:
         payload = f.read()
 
     return render_template(                 # return page_name and payload
-        "main",
-        page_name=this_page,
+        "main.html",
+        page_name=name,
         page_content=payload,
     )
