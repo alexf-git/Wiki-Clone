a989d0e83b3a4ec76e75e06923cdf15fa5d044b4
isaac <nunoisaac@google.com>
2021-06-08 21:10:34 +0000

    added tests for form_errors and validate_information function

--- wiki_test.py
+++ wiki_test.py
@@ -29,3 +29,41 @@ def test_El_Paso_Data(client):
 def test_no_path_city_request(client):
     resp = client.get("/city_request/El Paso, California")
     assert resp.status_code == 200
+
+
+@pytest.mark.parametrize(
+    ["content", "edit_description", "usr_name", "usr_email", "page_title","expected"],
+    [
+        (
+            "page_1 content img\ncity, state\nbody content","Added a page", "Jeff Bergman", "bunny@google.com", "Los Angeles, California", 0
+        ),
+        (
+            "","Page edit", "Mel Blanc", "daffy@gmail.com", "Portland, Oregon", -1
+        ),
+        (
+            "page_3 content img\ncity, state\nbody content","","Joe Dougherty", "porkey@gmail.com", "Los Angeles, California", -2
+        ),
+        (
+            "page_4 content img\ncity, state\nbody content","Page added", "", "sylvercat@looney.com", "Pratt, Kansas", -3
+        ),
+        (
+            "page_4 content img\ncity, state\nbody content", "Page edit","Greg Burson", "", "Anaheim, California", -4
+        ),
+        (
+            "page_5 content img\ncity, state\nbody content","Page edit","Joe Alaskey","fogleghorn@gmail.com",None, -5
+        )
+    ])
+def test_validate_information(content, edit_description, usr_name, usr_email, page_title,expected):
+    error_code = wiki.validate_information(content, edit_description, usr_name, usr_email, page_title)
+    assert error_code==expected
+
+
+@pytest.mark.parametrize(
+    ["error_code", "expected"],
+    [
+        (0,""), (-1,"error: post content empty"), (-2, "error: missing description"), (-3, "error: missing user name"),
+        (-4, "error: missing email"), (-5, "error: missing page title")
+    ])
+def test_form_errors(error_code, expected):
+    error_msg = wiki.form_errors(error_code)
+    assert error_msg==expected
