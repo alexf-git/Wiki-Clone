c884a8edc13336209c9996c95fd9d54aab3e6e7b
Nathanael Gastelum <ngastelum@google.com>
2021-06-08 20:49:16 +0000

    implemented edit route integration test

--- wiki_test.py
+++ wiki_test.py
@@ -1,4 +1,5 @@
 import pytest  # type: ignore
+import pathlib
 import wiki
 
 
@@ -29,3 +30,26 @@ def test_El_Paso_Data(client):
 def test_no_path_city_request(client):
     resp = client.get("/city_request/El Paso, California")
     assert resp.status_code == 200
+
+
+def test_edit_route_integration(client, monkeypatch):
+    test_dir = pathlib.Path(__file__).parent
+    test_dir = test_dir / "test_dir/"
+    monkeypatch.setattr(wiki, "current_dir", test_dir)
+    
+    client.post(f"/edit/add", data={
+        "text_area": """test_img.jpg
+test city, test state
+Test Morbid Event
+The test killer cheated on a test
+:;:
+testing
+discussion posts are required to not crash the page from a non iterable""",
+        "descript_edit": "test edit",
+        "fname": "Test Name",
+        "e_email": "testing@test.com"
+    })
+
+    response_get = client.get(f"/edit/{'test city, test state'}")
+    assert response_get.status_code == 200
+    assert b"test state" in response_get.data
