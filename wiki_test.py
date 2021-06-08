249def6c0706651e4f3da13969d3175a9057950c
Nathanael Gastelum <ngastelum@google.com>
2021-06-08 20:51:15 +0000

    implemented edit route integration test

--- wiki_test.py
+++ wiki_test.py
@@ -36,19 +36,22 @@ def test_edit_route_integration(client, monkeypatch):
     test_dir = pathlib.Path(__file__).parent
     test_dir = test_dir / "test_dir/"
     monkeypatch.setattr(wiki, "current_dir", test_dir)
-    
-    client.post(f"/edit/add", data={
-        "text_area": """test_img.jpg
+
+    client.post(
+        "/edit/add",
+        data={
+            "text_area": """test_img.jpg
 test city, test state
 Test Morbid Event
 The test killer cheated on a test
 :;:
 testing
 discussion posts are required to not crash the page from a non iterable""",
-        "descript_edit": "test edit",
-        "fname": "Test Name",
-        "e_email": "testing@test.com"
-    })
+            "descript_edit": "test edit",
+            "fname": "Test Name",
+            "e_email": "testing@test.com",
+        },
+    )
 
     response_get = client.get(f"/edit/{'test city, test state'}")
     assert response_get.status_code == 200
